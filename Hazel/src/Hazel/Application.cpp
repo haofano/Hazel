e46ba908798803bb0267d1d6b64e5dc71f937abf
haofano <wd1786@163.com>
2024-05-09 10:11:47 +0800

    #7 Window and GLFW

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,12 +1,13 @@
 #include "hzpch.h"
 #include "Application.h"
 #include "Events/ApplicationEvent.h"
-#include "Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
 	Application::Application()
 	{
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -16,17 +17,12 @@ namespace Hazel {
 
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication))
+		
+		while (m_Running)
 		{
-			HZ_TRACE(e);
-		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			HZ_TRACE(e);
-		}
-		while (true)
-		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
 	}
 }
