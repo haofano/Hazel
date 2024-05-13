831f4e1328a13ea57a451a6b74681dbf489b278c
haofano <wd1786@163.com>
2024-05-13 10:27:14 +0800

    #11 ImGui

---
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -0,0 +1,42 @@
+#include "hzpch.h"
+#include "ImGuiLayer.h"
+
+#include "imgui.h"
+#include "GLFW/glfw3.h"
+
+#include "Hazel/Application.h"
+
+namespace Hazel {
+
+	ImGuiLayer::ImGuiLayer()
+		: Layer("ImGuiLayer")
+	{
+
+	}
+
+	ImGuiLayer::~ImGuiLayer()
+	{
+
+	}
+
+	void ImGuiLayer::OnAttach()
+	{
+
+	}
+
+	void ImGuiLayer::OnDetach()
+	{
+
+	}
+
+	void ImGuiLayer::OnUpdate()
+	{
+
+	}
+
+	void ImGuiLayer::OnEvent(Event& event)
+	{
+
+	}
+
+}
