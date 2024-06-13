3dea5f88c8b86ea08a4fd90bf1fcf9d28e27f555
haofano <wd1786@163.com>
2024-06-13 10:29:40 +0800

    #29 Timestep

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -2,7 +2,7 @@
 #include "Application.h"
 #include "Input.h"
 
-#include "Hazel/Renderer/Renderer.h"
+#include <glfw/glfw3.h>
 
 namespace Hazel {
 
@@ -57,8 +57,12 @@ namespace Hazel {
 		
 		while (m_Running)
 		{
+			float time = (float)glfwGetTime();
+			Timestep timestep = time - m_LastFrameTime;
+			m_LastFrameTime = time;
+
 			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate();
+				layer->OnUpdate(timestep);
 
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
