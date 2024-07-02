1dc1056b208d6847622ff00f65f14b7a244f8e31
haofano <wd1786@163.com>
2024-07-02 09:57:18 +0800

    #37 Camera Controllers

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -11,6 +11,7 @@
 #include "Hazel/Input.h"
 #include "Hazel/KeyCodes.h"
 #include "Hazel/MouseButtonCodes.h"
+#include "Hazel/OrthographicCameraController.h"
 
 // ---------Renderer-----------
 #include "Hazel/Renderer/Renderer.h"
