d2ae8f7b8d0594f307166370e3048c967428e629
haofano <wd1786@163.com>
2024-10-18 15:51:01 +0800

    #84 Test GL_RED_INTEGER

--- Hazel/src/Hazel/Renderer/Framebuffer.h
+++ Hazel/src/Hazel/Renderer/Framebuffer.h
@@ -10,6 +10,7 @@ namespace Hazel {
 
 		// Color
 		RGBA8,
+		RED_INTERGER,
 
 		// Depth/stencil
 		DEPTH24STENCIL8,
@@ -54,6 +55,7 @@ namespace Hazel {
 		virtual void UnBind() = 0;
 
 		virtual void Resize(uint32_t width, uint32_t height) = 0;
+		virtual int ReadPixel(uint32_t attachmentIndex, int x, int y) = 0;
 
 		virtual uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const = 0;
 
